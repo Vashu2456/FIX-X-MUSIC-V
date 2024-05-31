b6bdf9d4bf1faeed884f48857abaccba738143ff
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-05-31 23:21:46 +0530

    𝐅𝐈𝐗𝐗 𝐌𝐔𝐒𝐈𝐂

---
+++ FIXXMUSIC/plugins/admins/unbanall.py
@@ -0,0 +1,31 @@
+from FIXXMUSIC import app
+from config import OWNER_ID
+from pyrogram import filters, enums
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from FIXXMUSIC.utils.fix_ban import admin_filter
+
+
+@app.on_message(filters.command("unbanall" , "vunbanall") & admin_filter)
+async def unban_all(_, msg):
+    chat_id = msg.chat.id
+    x = 0
+    bot = await app.get_chat_member(chat_id, BOT_ID)
+    bot_permission = bot.privileges.can_restrict_members == True
+    if bot_permission:
+        banned_users = []
+        async for m in app.get_chat_members(chat_id, filter=enums.ChatMembersFilter.BANNED):
+            banned_users.append(m.user.id)
+            try:
+                await app.unban_chat_member(chat_id, banned_users[x])
+                print(f"ᴜɴʙᴀɴɪɴɢ ᴀʟʟ ᴍᴄ ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ {m.user.mention}")
+                x += 1
+            except Exception:
+                pass
+    else:
+        await msg.reply_text("ᴇɪᴛʜᴇʀ ɪ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴛʜᴇ ʀɪɢʜᴛ ᴛᴏ ʀᴇsᴛʀɪᴄᴛ ᴜsᴇʀs ᴏʀ ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ɪɴ sᴜᴅᴏ ᴜsᴇʀs")
+
+@app.on_callback_query(filters.regex("^stop$"))
+async def stop_callback(_, query):
+    await query.message.delete()
+
+###

8d24ff06b4cb36f124b658a27f50ec2e7192ce2e
SKD-VASHU <165390858+Vashu2456@users.noreply.github.com>
2024-06-04 21:02:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Vashu2456/FIXXMUSICV",
+    "https://github.com/Vashu2456/FIX-X-MUSIC-V",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(

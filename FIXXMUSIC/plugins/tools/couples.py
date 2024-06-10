3cdf68f1c7f9e713f663373e6309534e72676291
SKD-VASHU <165390858+Vashu2456@users.noreply.github.com>
2024-06-10 17:41:27 +0530

    Update couples.py

--- FIXXMUSIC/plugins/tools/couples.py
+++ FIXXMUSIC/plugins/tools/couples.py
@@ -14,8 +14,8 @@ from FIXXMUSIC.mongo.couples_db import _get_image, get_couple
 POLICE = [
     [
         InlineKeyboardButton(
-            text="𓊈𒆜彡[𝐃𝚊𝚡𝚡 𝐂𝙲 𝐂𝙻𝚄𝙱 ]彡𒆜𓊉",
-            url=f"https://t.me/ALLTYPECC",
+            text="𓊈𒆜彡[ VASHU ]彡𒆜𓊉",
+            url=f"https://t.me/i_am_vashu",
         ),
     ],
 ]
